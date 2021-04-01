26342ab51228618a413585601051e677b58f9836
teyz <contact@bastienrigaud.fr>
2021-04-01 15:05:17 +0200

    ADD: findAllBook and bookid

---
+++ src/Controller/BooksController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Livres;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class BooksController extends AbstractController
+{
+    /**
+     * @Route("/books/all", name="allBooks")
+     */
+    public function getAllBooks(SerializerInterface $serializer): Response
+    {
+        $repository = $this->getDoctrine()->getRepository(Livres::class);
+        $books = $repository->findAll();
+
+        return new Response($serializer->serialize($books, 'json'));
+    }
+
+    /**
+     * @Route("/books/{id}", name="singleBook")
+     */
+    public function getSingleBook($id, SerializerInterface $serializer): Response
+    {
+        $repository = $this->getDoctrine()->getRepository(Livres::class);
+        $book = $repository->find($id);
+
+        return new Response($serializer->serialize($book, 'json'));
+    }
+
+     /**
+     * @Route("/books/search/{research}", name="researchBook")
+     */
+    public function getResearchBook($research, SerializerInterface $serializer): Response
+    {
+        $repository = $this->getDoctrine()->getRepository(Livres::class);
+        $search = $repository->findBy(["auteur" => $research]);
+        $search = $repository->findBy(["isbn_code" => $research]);
+        $search = $repository->findBy(["name" => $research]);
+        $search = $repository->findBy(["id" => $research]);
+        if($search === []){
+            dd("test");
+        }
+
+        return new Response($serializer->serialize($search, 'json'));
+    }
+
+
+}
