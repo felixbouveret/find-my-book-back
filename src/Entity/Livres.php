89b2aba1f03b6039626a216dab06255a58381f17
teyz <contact@bastienrigaud.fr>
2021-05-29 00:25:47 +0200

    Features: add algolia bundle

--- src/Entity/Livres.php
+++ src/Entity/Livres.php
@@ -18,6 +18,7 @@ class Livres
 {
     /**
      * @ORM\Id
+     * @Groups({"searchable"})
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      * @Groups({"show_commentary", "show_notes", "show_likes"})
@@ -26,30 +27,35 @@ class Livres
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"searchable"})
      * @Groups({"show_commentary", "show_notes", "show_likes"})
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"searchable"})
      * @Groups({"show_commentary", "show_notes", "show_likes"})
      */
     private $isbn_code;
 
     /**
      * @ORM\Column(type="text", nullable=true)
+     * @Groups({"searchable"})
      * @Groups({"show_commentary", "show_notes", "show_likes"})
      */
     private $synopsis;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"searchable"})
      * @Groups({"show_commentary", "show_notes", "show_likes"})
      */
     private $auteur;
 
     /**
      * @ORM\ManyToMany(targetEntity=User::class, mappedBy="likes")
+     * @Groups({"searchable"})
      * @Groups({"show_likes"})
      */
     private $likes;
@@ -57,6 +63,7 @@ class Livres
     /**
      * @ORM\OneToMany(targetEntity=Notes::class, mappedBy="livre", orphanRemoval=true)
      * @Groups({"show_notes"})
+     * @Groups({"searchable"})
      */
     private $notes;
 
@@ -64,12 +71,14 @@ class Livres
      * @ORM\OneToMany(targetEntity=Commentaires::class, mappedBy="livre", orphanRemoval=true)
      * @Groups({"show_commentary"})
      * @MaxDepth(2)
+     * @Groups({"searchable"})
      */
     private $commentaires;
 
     /**
      * @ORM\Column(type="integer")
      * @Groups({"show_commentary", "show_notes", "show_likes"})
+     * @Groups({"searchable"})
      */
     private $category;
 
@@ -80,11 +89,17 @@ class Livres
         $this->commentaires = new ArrayCollection();
     }
 
+    /**
+     * @Groups({"searchable"})
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @Groups({"searchable"})
+     */
     public function getName(): ?string
     {
         return $this->name;
@@ -97,6 +112,9 @@ class Livres
         return $this;
     }
 
+    /**
+     * @Groups({"searchable"})
+     */
     public function getIsbnCode(): ?string
     {
         return $this->isbn_code;
@@ -109,6 +127,9 @@ class Livres
         return $this;
     }
 
+    /**
+     * @Groups({"searchable"})
+     */
     public function getSynopsis(): ?string
     {
         return $this->synopsis;
@@ -121,6 +142,9 @@ class Livres
         return $this;
     }
 
+    /**
+     * @Groups({"searchable"})
+     */
     public function getAuteur(): ?string
     {
         return $this->auteur;
